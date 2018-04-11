d475fe8abbd9f86d7d76d35052500fc546f83869
zhenghaiyong <zhenghaiyong@gmail.com>
2018-04-11 12:01:55 +0800

    20180411

---
+++ week6/crlf.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hahaha" << "\r" << "xixi" ;
+    cout << endl << endl;
+    cout << "hahaha" << "\n" << "xixi" ;
+    cout << endl << endl;
+
+    return 0;
+}
